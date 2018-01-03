4654cbf755eccac5edcc06fb6d8f26c17ab2bf3b
Jeff Carbonella <jeff.carbonella@gusto.com>
2018-01-03 13:39:08 -0700

    Get relative path to files

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-module.exports = results => {
+const path = require('path');
+
+module.exports = (results) => {
   const rules = results.reduce((memo, { filePath, messages }) => {
     messages.forEach(({ ruleId }) => {
       // eslint-disable-next-line no-param-reassign
       memo[ruleId] = memo[ruleId] || [];
-      memo[ruleId].push(filePath.replace(`${__dirname}/`, ""));
+      memo[ruleId].push(path.relative('.', filePath));
     });
     return memo;
   }, {});
   const sortedRuleIds = Object.keys(rules).sort();
 
-  const overrides = sortedRuleIds.map(ruleId => ({
-    rules: { [ruleId]: "off" },
-    files: [...new Set(rules[ruleId])].sort()
+  const overrides = sortedRuleIds.map((ruleId) => ({
+    rules: { [ruleId]: 'off' },
+    files: [...new Set(rules[ruleId])].sort(),
   }));
 
   return `module.exports = ${JSON.stringify({ overrides }, null, 2)}`;

19c3c80c78506863cde8548143dd83e02dbfb8be
Jeff Carbonella <jeff.carbonella@gusto.com>
2018-01-03 10:16:52 -0700

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 100, // default: 80
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true, // default: false
+  trailingComma: "all", // default: 'none'
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: "always" // default: 'avoid'
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // parser: babylon,
+  // filepath: None,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: 'preserve',
+};
+
